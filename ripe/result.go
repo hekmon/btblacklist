ebe8609a53dbacb94674e77f358d2caa5d9ee832
Hekmon <edouardhur@gmail.com>
2019-05-24 18:26:22 +0200

    simplify the strings values

--- ripe/result.go
+++ ripe/result.go
@@ -1,5 +1,11 @@
 package ripe
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
 type answer struct {
 	Result answerResult `json:"result"`
 	Lists  []lst        `json:"lsts"`
@@ -14,7 +20,7 @@ type answerResult struct {
 
 type doc struct {
 	Doc struct {
-		Strings []answerString `json:"strs"`
+		Strings answerStrings `json:"strs"`
 	} `json:"doc"`
 }
 
@@ -24,7 +30,7 @@ type lst struct {
 		Arrs    []answerArr     `json:"arrs"`
 		Ints    []answerInteger `json:"ints"`
 		Lists   []lst           `json:"lsts"`
-		Strings []answerString  `json:"strs"`
+		Strings answerStrings   `json:"strs"`
 	} `json:"lst"`
 }
 
@@ -48,9 +54,30 @@ type answerInteger struct {
 	} `json:"int"`
 }
 
-type answerString struct {
-	String struct {
-		Name  string `json:"name"`
-		Value string `json:"value"`
-	} `json:"str"`
+type answerStrings map[string][]string
+
+func (as *answerStrings) UnmarshalJSON(data []byte) (err error) {
+	// Unmarshall the fake JSON-XML payload
+	var tmp []struct {
+		String struct {
+			Name  string `json:"name"`
+			Value string `json:"value"`
+		} `json:"str"`
+	}
+	decoder := json.NewDecoder(bytes.NewBuffer(data))
+	decoder.DisallowUnknownFields()
+	if err = decoder.Decode(&tmp); err != nil {
+		err = fmt.Errorf("can't unmarshall strings list into the tmp struct: %v", err)
+		return
+	}
+	// Simplify it
+	var exist bool
+	*as = make(map[string][]string, len(tmp))
+	for _, value := range tmp {
+		if _, exist = (*as)[value.String.Name]; !exist {
+			(*as)[value.String.Name] = make([]string, 0, 1)
+		}
+		(*as)[value.String.Name] = append((*as)[value.String.Name], value.String.Value)
+	}
+	return
 }
